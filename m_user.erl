bfa53630994054eca23bb63b564cffd9be539277
HIROE Shin <hiroe.orz@gmail.com>
2011-06-13 11:43:01 +0900

    modify users process name

--- m_user.erl
+++ m_user.erl
@@ -8,7 +8,7 @@
 -export([send_message/2, get_message/2]).
 
 start(UserName)->
-    register(UserName, spawn(?MODULE, init, [UserName])).
+    register(process_name(UserName), spawn(?MODULE, init, [UserName])).
 
 init(UserName)->
     User = user_db:lookup_name(UserName),
@@ -33,13 +33,13 @@ get_message(UserName, MessageId)->
 %%
 
 call(UserName, Name, Args)->
-    UserName ! {request, self(), Name, Args},
+    process_name(UserName) ! {request, self(), Name, Args},
     receive
 	{reply, Result} -> Result
     end.
 
 reference_call(UserName, Name, Args)->
-    UserName ! {ref_request, self(), Name, Args},
+    process_name(UserName) ! {ref_request, self(), Name, Args},
     receive
 	{reply, Result} -> Result
     end.
@@ -65,8 +65,9 @@ loop({User, MessageDB_Pid}=State) ->
 	    loop(State)
     end.
 
-handle_stop({_User, MessageDB_Pid})->
-    message_db:stop(MessageDB_Pid).
+handle_stop({_, MessageDB_Pid})->
+    message_db:stop(MessageDB_Pid),
+    {stop, self()}.
 
 handle_request(latest_message, [{User, _}])->
     message_db:get_latest_message(User#user.name);
@@ -76,3 +77,11 @@ handle_request(send_message, [{_User, MessageDB_Pid}, Text])->
 
 handle_request(get_message, [{_User, MessageDB_Pid}, MessageId]) ->
     message_db:get_message(MessageDB_Pid, MessageId).
+
+%%
+%% @doc local functions.
+%%
+
+process_name(UserName)-> 
+    list_to_atom(string:concat("m_user_", atom_to_list(UserName))).
+
