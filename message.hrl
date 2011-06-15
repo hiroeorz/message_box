1583f9bf5413caeacddedcd7c86c9b1a97f81fb8
HIROE Shin <hiroe.orz@gmail.com>
2011-06-15 18:37:54 +0900

    create Makefile

--- message.hrl
+++
@@ -1,6 +0,0 @@
-%% File : message.hrl
-%% Description : Include file for message_db
-
--record(message, {id,         %% int()
-		  message_id, %% int()
-		  text}).     %% string()
--- user.hrl
+++
@@ -1,6 +0,0 @@
-%% File : usr.hrl
-%% Description : Include file for user_db
-
--record(user, {id,               %int()
-	       status = enabled, %atom()
-	       name}).           %term()
